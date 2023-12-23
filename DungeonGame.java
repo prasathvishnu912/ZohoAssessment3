2d6e31ec8e04e93133e57c2d6e498336863be816
prasathvishnu912 <149511892+prasathvishnu912@users.noreply.github.com>
2023-12-23 13:51:46 +0530

    Update DungeonGame.java
    Question4 uploaded.

--- DungeonGame.java
+++ DungeonGame.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class DungeonGameQ3 {
+public class DungeonGameQ4 {
 
-	static List<String> pathList = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,6 +14,7 @@ public class DungeonGameQ3 {
 		int row = sc.nextInt();
 		int col = sc.nextInt();
 		sc.nextLine();
+		
 		System.out.println("Enter adventure row and column!");
 		int aI = sc.nextInt();
 		int aJ = sc.nextInt();
@@ -24,26 +24,66 @@ public class DungeonGameQ3 {
 		int mI = sc.nextInt();
 		int mJ = sc.nextInt();
 		
+		System.out.println("Enter Trigger row and column!");
+		int tI = sc.nextInt();
+		int tJ = sc.nextInt();
+		
 		System.out.println("Enter Gold row and column!");
 		int gI = sc.nextInt();
 		int gJ = sc.nextInt();
 		
 		
 		int monsMinSteps = getMonsMinSteps(row, col, mI, mJ, gI, gJ);
-		int advMinSteps = getAdvMinSteps(row, col, aI, aJ, gI, gJ);
+		int advMinStepsToG = getAdvMinStepsToGold(row, col, aI, aJ, gI, gJ);
+		int advMinStepsToTrig = getAdvMinStepsToTrigger(row, col, aI, aJ, tI, tJ);
+		int advMinSteps = advMinStepsToTrig<monsMinSteps ? advMinStepsToG<monsMinSteps ? advMinStepsToG : advMinStepsToTrig: advMinStepsToG;
+		
 		if(monsMinSteps<advMinSteps) {
 			System.out.println("Minimum number of steps: "+advMinSteps);
 		}else {
-			for(String s:pathList) {
-				System.out.print(s+"->");
-			}
-			System.out.println();
 			System.out.println("Minimum Number of stps taken: "+advMinSteps);
 		}
 	}
 
 	
 	
+	private static int getAdvMinStepsToTrigger(int row, int col, int aI, int aJ, int gI, int gJ) {
+
+		int maxStep = row + col -1;
+		int i=0;
+		
+		int cI = aI;
+		int cJ = aJ;
+		
+		while(i<=maxStep) {
+			if(cI==gI&&cJ==gJ) {
+				break;
+			}else {
+				if(cJ==gJ) {
+					
+					if(cI>gI) {
+						cI--;
+					}else {
+						cI++;
+					}
+					i++;
+				}
+				else if(cJ<gJ) {
+					cJ++;
+					i++;
+				}else {
+					cJ--;
+					i++;
+				}
+			}
+		}
+	
+		
+		return i;
+	}
+
+
+
 	private static int getMonsMinSteps(int row, int col, int mI, int mJ, int gI, int gJ) {
 		// TODO Auto-generated method stub
 		int maxStep = row + col -1;
@@ -79,7 +119,7 @@ public class DungeonGameQ3 {
 
 
 
-	private static int getAdvMinSteps(int row, int col, int aI, int aJ, int gI, int gJ) {
+	private static int getAdvMinStepsToGold(int row, int col, int aI, int aJ, int gI, int gJ) {
 		// TODO Auto-generated method stub
 		
 		int maxStep = row + col -1;
@@ -92,7 +132,6 @@ public class DungeonGameQ3 {
 			if(cI==gI&&cJ==gJ) {
 				break;
 			}else {
-				pathList.add("("+cI+","+cJ+")");
 				if(cJ==gJ) {
 					
 					if(cI>gI) {
@@ -111,8 +150,6 @@ public class DungeonGameQ3 {
 				}
 			}
 		}
-		
-		pathList.add("("+cI+","+cJ+")");
 		
 		return i;
 	}
