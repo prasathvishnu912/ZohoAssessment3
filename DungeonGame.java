3530fa2f12714ce8f3b22b699f78eb2aa2040dcc
prasathvishnu912 <149511892+prasathvishnu912@users.noreply.github.com>
2023-12-23 12:19:32 +0530

    DungeonGame Question2
    DungeonGame Question2 upload.

--- DungeonGame.java
+++ DungeonGame.java
@@ -14,17 +14,60 @@ public class DungeonGame {
 		int aI = sc.nextInt();
 		int aJ = sc.nextInt();
 		sc.nextLine();
+		System.out.println("Enter Monster row and column!");
+		int mI = sc.nextInt();
+		int mJ = sc.nextInt();
 		System.out.println("Enter Gold row and column!");
 		int gI = sc.nextInt();
 		int gJ = sc.nextInt();
 		
 		
-		int minSteps = getMinSteps(row, col, aI, aJ, gI, gJ);
-		System.out.println(minSteps);
+		int monsMinSteps = getMonsMinSteps(row, col, mI, mJ, gI, gJ);
+		int advMinSteps = getAdvMinSteps(row, col, aI, aJ, gI, gJ);
+		if(monsMinSteps<advMinSteps) {
+			System.out.println("No possible solution");
+		}else {
+			System.out.println("Minimum number of steps: "+advMinSteps);
+		}
 		
 	}
 
-	private static int getMinSteps(int row, int col, int aI, int aJ, int gI, int gJ) {
+	private static int getMonsMinSteps(int row, int col, int mI, int mJ, int gI, int gJ) {
+		// TODO Auto-generated method stub
+		int maxStep = row + col -1;
+		int i=0;
+		
+		int cI = mI;
+		int cJ = mJ;
+		
+		while(i<=maxStep) {
+			if(cI==gI&&cJ==gJ) {
+				break;
+			}else {
+				if(cJ==gJ) {
+					if(cI>gI) {
+						cI--;
+					}else {
+						cI++;
+					}
+					i++;
+				}
+				else if(cJ<gJ) {
+					cJ++;
+					i++;
+				}else {
+					cJ--;
+					i++;
+				}
+			}
+		}
+		return i;
+		
+	}
+
+
+
+	private static int getAdvMinSteps(int row, int col, int aI, int aJ, int gI, int gJ) {
 		// TODO Auto-generated method stub
 		
 		int maxStep = row + col -1;
@@ -60,6 +103,5 @@ public class DungeonGame {
 		return i;
 	}
 
-	
 
 }
